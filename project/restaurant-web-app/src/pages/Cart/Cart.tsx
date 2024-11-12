63ea51d5bd1033a67b3c2e68056c165c298f79d2
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-11-12 17:07:10 +0100

    change quantity item values in the cart page

--- project/restaurant-web-app/src/pages/Cart/Cart.tsx
+++ project/restaurant-web-app/src/pages/Cart/Cart.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
-import type { Item } from "../../types/types";
 import { useState } from "react";
+import type { Item } from "../../types/types";
 
 interface CartItem extends Item {
   quantity: number;
@@ -12,21 +12,71 @@ export default function CartPage() {
 
   const [cartItems, setCartItems] = useState<CartItem[]>(receivedData);
 
+  function handlePlusQuantity(id: number, q: number): void {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, quantity: q + 1 } : item
+      )
+    );
+  }
+
+  function handleLessQuantity(id: number, q: number): void {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, quantity: q - 1 } : item
+      )
+    );
+  }
+
   return (
-    <main>
-      <h2>Cart page</h2>
-      <p>{cartItems.length}</p>
-      <ol>
-        {cartItems.map((i) => (
-          <li key={i.id}>
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <button> + </button>
-              <p>antal</p>
-              <button> - </button>
+    <>
+      <header> 🛒 </header>
+      <main>
+        <section>
+          <ol>
+            {cartItems.map((i) => (
+              <li key={i.id}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <p>{i.name}</p>
+                    <p>{i.price}</p>
+                  </div>
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <button
+                      onClick={() => handlePlusQuantity(i.id, i.quantity)}
+                    >
+                      +
+                    </button>
+                    <p>{i.quantity}</p>
+                    <button
+                      onClick={() => handleLessQuantity(i.id, i.quantity)}
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </section>
+        <section>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div>
+              <h2>TOTALT</h2>
+              <p>Inc 20% moms</p>
             </div>
-          </li>
-        ))}
-      </ol>
-    </main>
+            <div>
+              <h2>Price</h2>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
